c2c3f1db2d8c1a6dc9e52e046390501fbbacc9e1
ragOP <najmiraghib@gmail.com>
2024-08-02 07:17:44 +0530

    hbjh

--- src/pages/7.tsx
+++ src/pages/7.tsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast, cssTransition } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Head_bg from "../assets/hero5.png";
 import HeaderLogo from "../assets/sawal.png";
-import aa from "../assets/sam.png";
+import aa from "../assets/last.png";
 import Headline from "../assets/headline_spandeb1.png";
 import {  Link } from 'react-router-dom';
 // google tag manager
@@ -344,7 +344,7 @@ export default function Fifth_SP() {
     <div>
      {/* <ToastContainer /> */}
      <div  className="top-sticky-blue-test2" id="top">
-     <img src={aa} alt="Description of image" style={{ height: 'auto', maxWidth: '35%' }} />
+     <img src={aa} alt="Description of " style={{ height: '74%', maxWidth: '74%' }} />
   </div>
 
       {step === "process" ? (
